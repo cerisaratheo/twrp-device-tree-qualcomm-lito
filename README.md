4857e4552e4f5ade1f1bcd02786aee3a031717f1
cerisaratheo <cerisaratheo@gmail.com>
2023-05-06 22:03:22 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# twrp-device-tree-qualcomm-lito
+# Android device tree for qti Lito for arm64 (lito)
+
+```
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ omni_lito.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from lito device
+$(call inherit-product, device/qualcomm/lito/device.mk)
+
+PRODUCT_DEVICE := lito
+PRODUCT_NAME := omni_lito
+PRODUCT_BRAND := qti
+PRODUCT_MODEL := Lito for arm64
+PRODUCT_MANUFACTURER := qualcomm
+
+PRODUCT_GMS_CLIENTID_BASE := android-qualcomm
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="lito-user 12 RKQ1.211119.001 1677729784975 release-keys"
+
+BUILD_FINGERPRINT := qti/lito/lito:12/RKQ1.211119.001/1677729784975:user/release-keys
