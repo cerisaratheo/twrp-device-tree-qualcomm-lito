87bbbfa05dfa7427fd10a0d0ca5ebd17dc0f5224
cerisaratheo <cerisaratheo@gmail.com>
2023-05-07 12:13:30 +0200

    test fix obsolete add_lunch_combo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_lito-user
-add_lunch_combo omni_lito-userdebug
-add_lunch_combo omni_lito-eng
+# add_lunch_combo omni_lito-user
+# add_lunch_combo omni_lito-userdebug
+# add_lunch_combo omni_lito-eng
+
+COMMON_LUNCH_CHOICES += omni_lito-user
+COMMON_LUNCH_CHOICES += omni_lito-userdebug
+COMMON_LUNCH_CHOICES += omni_lito-eng
+
