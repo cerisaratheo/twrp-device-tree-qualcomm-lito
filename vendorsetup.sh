22456a03638c2177cdecaa27679265ccb726f413
cerisaratheo <cerisaratheo@gmail.com>
2023-05-07 14:46:47 +0200

    test

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,12 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# add_lunch_combo omni_lito-user
-# add_lunch_combo omni_lito-userdebug
-# add_lunch_combo omni_lito-eng
-
-COMMON_LUNCH_CHOICES := \
-    omni_lito-user \
-    omni_lito-userdebug \
-    omni_lito-eng
+add_lunch_combo omni_lito-user
+add_lunch_combo omni_lito-userdebug
+add_lunch_combo omni_lito-eng
 
